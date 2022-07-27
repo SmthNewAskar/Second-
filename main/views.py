f7e001d447fabf727c08c5fee2a4483da2d62cba
SmthNewAskar <allaniyazovaskar@gmail.com>
2022-07-27 12:42:40 +0500

    First Save

---
+++ main/views.py
@@ -0,0 +1,30 @@
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from main.forms import TaskForm
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница cайта', 'tasks': tasks})
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == "POST": 
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма была неверной"
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+        
+    }
+    return render(request, 'main/create.html', context)
+
+
